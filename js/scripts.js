5c8eddd4415b9f8ab1970c44869d6a2638da3ff6
Bryant Ortega <kalicorose@gmail.com>
2023-01-28 00:16:06 -0800

    added pokemonList to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Pokémon data to display in app//
+let pokemonList = [
+    { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
+    { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+    { name: "Lapras", height: 2.5, type: ["Ice", "Water"] },
+];
