ddb192e58735881e27707ff646cb76db01e210aa
Bryant Ortega <kalicorose@gmail.com>
2023-01-28 23:13:36 -0800

    added normalize link to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ let pokemonList = [
     { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
     { name: "Lapras", height: 2.5, type: ["Ice", "Water"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(
+        pokemonList[i].name + " (height: " + pokemonList[i].height + ")"
+    );
+}
