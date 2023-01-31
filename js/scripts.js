da4cd61321601728154d2936a8185b2603b86367
Bryant Ortega <kalicorose@gmail.com>
2023-01-30 22:10:09 -0800

    added IIFE and replaced loop in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
-// Pokémon data to display in app//
-let pokemonList = [
-    { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
-    { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
-    { name: "Lapras", height: 2.5, type: ["Ice", "Water"] },
-];
-//loop to print the pokemon objects with heigh condition to the DOM//
+// wrapping Pokémon Data in IIFE format //
+let pokemonRepository = (function () {
+    // Pokémon data to display in app//
+    let pokemonList = [
+        { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
+        { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+        { name: "Lapras", height: 2.5, type: ["Ice", "Water"] },
+    ];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
+
+/*the original for loop 
+loop to print the pokemon objects with height condition to the DOM//
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.7) {
         document.write(
@@ -18,4 +36,18 @@ for (let i = 0; i < pokemonList.length; i++) {
             pokemonList[i].name + " (height: " + pokemonList[i].height + "m)<p>"
         );
     }
-}
+} */
+
+// New .forEach() loop //
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 1.7) {
+        document.write(
+            pokemon.name +
+                " (height: " +
+                pokemon.height +
+                "m) - Wow that's big! <p>"
+        );
+    } else {
+        document.write(pokemon.name + " (height: " + pokemon.height + "m)<p>");
+    }
+});
