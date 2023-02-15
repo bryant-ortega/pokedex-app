a5c847a6c0fffe90262d2aec707d696cdeac1b4d
Bryant Ortega <kalicorose@gmail.com>
2023-02-14 21:57:02 -0800

    added showModal repository to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -96,7 +96,106 @@ let pokemonRepository = (function () {
         loadDetails: loadDetails,
     };
 })();
+// modal repository IIFE //
+(function () {
+    function showModal(title, text) {
+        let modalContainer = document.querySelector("#modal-container");
+        modalContainer.innerHTML = "";
+        let modal = document.createElement("div");
+        modal.classList.add("modal");
 
+        let closeButtonElement = document.createElement("button");
+        closeButtonElement.classList.add("modal-close");
+        closeButtonElement.innerText = "Close";
+        closeButtonElement.addEventListener("click", hideModal);
+
+        let titleElement = document.createElement("h1");
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement("p");
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add("is-visible");
+        modalContainer.addEventListener("click", e => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+        });
+    }
+    let dialogPromiseReject;
+
+    function hideModal() {
+        //we have defined modalContainer here //
+        let modalContainer = document.querySelector("#modal-container");
+        // makes the container visible //
+        modalContainer.classList.remove("is-visible");
+
+        if (dialogPromiseReject) {
+            dialogPromiseReject();
+            dialogPromiseReject = null;
+        }
+    }
+    function showDialog(title, text) {
+        showModal(title, text);
+
+        // We have defined modalContainer here //
+        let modalContainer = document.querySelector("#modal-container");
+
+        // We want to add a confirm and cancel button to the modal so this defines modal element //
+        let modal = modalContainer.querySelector(".modal");
+        // defines confirm button //
+        let confirmButton = document.createElement("button");
+        confirmButton.classList.add("modal-confirm");
+        confirmButton.innerText = "Confirm";
+        // defines cancel button //
+        let cancelButton = document.createElement("button");
+        cancelButton.classList.add("modal-cancel");
+        cancelButton.innerText = "Cancel";
+        // placement of confirm and cancel buttons //
+        modal.appendChild(confirmButton);
+        modal.appendChild(cancelButton);
+        // We want to focus the confirmButton so that the user can simply press Enter
+        confirmButton.focus();
+        // wire up buttons by returning a promise //
+        return new Promise((resolve, reject) => {
+            cancelButton.addEventListener("click", hideModal);
+            confirmButton.addEventListener("click", () => {
+                hideModal();
+                resolve();
+            });
+            dialogPromiseReject = reject;
+        });
+    }
+    window.addEventListener("keydown", e => {
+        let modalContainer = document.querySelector("#modal-container");
+        if (
+            e.key === "Escape" &&
+            modalContainer.classList.contains("is-visible")
+        ) {
+            hideModal();
+        }
+    });
+
+    document.querySelector("#show-modal").addEventListener("click", () => {
+        showModal("Test Modal Title", "This is the Modal text");
+    });
+    document.querySelector("#show-dialog").addEventListener("click", () => {
+        showDialog("Confirm action", "Are you sure you want to do this?").then(
+            function () {
+                alert("confirmed!");
+            },
+            () => {
+                alert("not confirmed");
+            }
+        );
+    });
+})();
 pokemonRepository.loadList().then(function () {
     // Now the data is loaded!
     pokemonRepository.getAll().forEach(function (pokemon) {
